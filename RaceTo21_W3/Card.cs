09f0065e3b2e1f612221366507b57e05603b33e0
MaskMan-DU <duyiran19385@gmail.com>
2023-02-02 09:49:03 -0500

    Add project files.

---
+++ RaceTo21_W3/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTo21
+{
+    public class Card
+    {
+        public string id;
+        public string fullName;
+        public int value;
+
+        public Card (string shortName,string longName, int point)
+        {
+            id = shortName;
+            fullName = longName;
+            value = point;
+        }
+
+    }
+}
